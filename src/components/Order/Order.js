2fd095fcce8818af7c58460039ca4b699cbe8d31
MehediHasan95 <imehedihasan95@gmail.com>
2022-12-01 11:04:56 +0600

    FINAL

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,36 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase.init";
-import { AiFillDelete } from "react-icons/ai";
+import OrderDetails from "../OrderDetails/OrderDetails";
 
 const Order = () => {
-  const [order, setOrder] = useState();
-  const [payment, setPayment] = useState();
+  const [users, setUsers] = useState();
+  const [order, setOrder] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  console.log(payment);
   useEffect(() => {
     onSnapshot(
-      collection(db, "order"),
+      collection(db, "userList"),
       (snapshot) => {
-        // console.log(snapshot);
         const getValue = snapshot.docs.map((e) => e.data());
-        setOrder(getValue);
+        setUsers(getValue);
         setLoading(true);
       },
       (error) => {
-        console.log(error);
-      }
-    );
-    onSnapshot(
-      collection(db, "paymentInfo"),
-      (snapshot) => {
-        const getValue = snapshot.docs.map((e) => e.data());
-        setPayment(getValue);
-        setLoading(true);
-      },
-      (error) => {
-        console.log(error);
+        alert(error);
       }
     );
   }, []);
@@ -40,12 +27,25 @@ const Order = () => {
     await deleteDoc(doc(db, "paymentInfo", id));
   };
 
+  const handleUserOrder = async (userId) => {
+    onSnapshot(
+      collection(db, `order/${userId}/userOrder`),
+      (snapshot) => {
+        const getValue = snapshot.docs.map((e) => e.data());
+        setOrder(getValue);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+
   return (
     <div className="common-line">
       {loading === false ? (
         <div className="d-flex justify-content-center align-items-center h-100">
-          <div class="spinner-grow" role="status">
-            <span class="visually-hidden">Loading...</span>
+          <div className="spinner-grow" role="status">
+            <span className="visually-hidden">Loading...</span>
           </div>
         </div>
       ) : (
@@ -53,58 +53,13 @@ const Order = () => {
       )}
 
       {loading === true &&
-        order.map((element) => {
-          const { name, email, address, phone, cart, id } = element;
-
-          return (
-            <div className="border  m-3 p-3">
-              <div className="d-flex justify-content-between">
-                <div className="d-flex">
-                  <div className="ms-4">
-                    <h3>{name}</h3>
-                    <h6>E-mail: {email}</h6>
-                    <h6>Phone: {phone}</h6>
-                    <p>Address: {address}</p>
-                    <hr />
-                    {payment.map((e) => (
-                      <div>
-                        <small>Card: {e.card}</small>
-                        <h5>Paid: BDT {e.paid}</h5>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-                <div>
-                  {cart.map((item) => {
-                    const { id, productName, price } = item.product;
-                    return (
-                      <div
-                        key={item.pId}
-                        className="order-item border mb-2 p-2"
-                      >
-                        <h6>
-                          <span className="text-warning">PID:</span> {id}
-                        </h6>
-                        <p>
-                          <span className="text-warning">Name:</span>{" "}
-                          {productName}
-                        </p>
-                        <p>
-                          <span className="text-warning">Price:</span> BDT{" "}
-                          {price}
-                        </p>
-                      </div>
-                    );
-                  })}
-                </div>
-                <AiFillDelete
-                  onClick={() => handleRemove(id)}
-                  className="my-auto delete-btn me-3"
-                />
-              </div>
-            </div>
-          );
-        })}
+        users.map((element) => (
+          <OrderDetails
+            orderDetail={element}
+            handleUserOrder={handleUserOrder}
+            order={order}
+          />
+        ))}
     </div>
   );
 };
--- src/components/ViewProduct/ViewProduct.js
+++ src/components/ViewProduct/ViewProduct.js
@@ -1,12 +1,28 @@
-import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../../firebase.init";
-import { AiFillDelete } from "react-icons/ai";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import noImage from "../../image/noImage.jpg";
 
 const ViewProduct = () => {
   const [view, setView] = useState();
+  const [update, setUpdate] = useState({});
   const [loading, setLoading] = useState(false);
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+
+  const handleNameBlur = (event) => {
+    setProductName(event.target.value);
+  };
+  const handlePriceBlur = (event) => {
+    setProductPrice(event.target.value);
+  };
 
   useEffect(() => {
     onSnapshot(
@@ -24,6 +40,28 @@ const ViewProduct = () => {
 
   const handleRemove = async (id) => {
     await deleteDoc(doc(db, "products", id));
+  };
+
+  const handleEditProduct = async (id) => {
+    onSnapshot(
+      collection(db, "products"),
+      (snapshot) => {
+        const getValue = snapshot.docs.map((e) => e.data());
+        const selectItem = getValue.find((e) => e.id === id);
+        setUpdate(selectItem);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+
+  const handleUpdate = async (id) => {
+    await updateDoc(doc(db, "products", id), {
+      productName: productName,
+      price: productPrice,
+    });
+    alert("Update Successfull. 😍");
   };
 
   return (
@@ -44,16 +82,16 @@ const ViewProduct = () => {
           return (
             <div className="border  m-3 p-3">
               <div className="d-flex justify-content-between">
-                <div className="d-flex">
+                <div className="d-flex w-100">
                   <img
                     src={
                       img.match(
-                        /^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gim
+                        /^http[^?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gim
                       ) != null
                         ? img
                         : noImage
                     }
-                    alt=""
+                    alt={img}
                     className="w-25"
                   />
                   <div className="ms-4">
@@ -62,10 +100,76 @@ const ViewProduct = () => {
                     <p>Category: {category}</p>
                   </div>
                 </div>
+
+                <AiFillEdit
+                  onClick={() => handleEditProduct(id)}
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  className="my-auto delete-btn me-3"
+                />
+
                 <AiFillDelete
                   onClick={() => handleRemove(id)}
                   className="my-auto delete-btn me-3"
                 />
+
+                <div
+                  class="modal fade text-dark"
+                  id="exampleModal"
+                  tabindex="-1"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">
+                          {update.id}
+                        </h1>
+                        <button
+                          type="button"
+                          class="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div class="modal-body">
+                        <input
+                          onBlur={handleNameBlur}
+                          type="text"
+                          placeholder={update.productName}
+                          className="input-filed border"
+                          required
+                        />
+                        <br />
+                        <input
+                          onBlur={handlePriceBlur}
+                          type="text"
+                          placeholder={update.price}
+                          className="input-filed border"
+                          required
+                        />
+                      </div>
+                      <div class="modal-footer">
+                        <button
+                          type="button"
+                          class="btn btn-danger"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button
+                          onClick={() => handleUpdate(update.id)}
+                          type="button"
+                          class="btn btn-success"
+                          data-bs-dismiss="modal"
+                        >
+                          Update
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           );
